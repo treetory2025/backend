36f511a60131a582c2e78b762d7a9022fb3d9cf9
Cutaku <mmzz0219@gmail.com>
2025-11-28 11:30:37 +0900

    initial commit

---
+++ treetory/src/main/java/site/treetory/TreetoryApplication.java
@@ -0,0 +1,13 @@
+package site.treetory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreetoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreetoryApplication.class, args);
+	}
+
+}
---
+++ treetory/src/test/java/site/treetory/TreetoryApplicationTests.java
@@ -0,0 +1,13 @@
+package site.treetory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreetoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
