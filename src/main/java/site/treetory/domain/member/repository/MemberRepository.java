b223269cd68334988bf2e03f43f2c180ab34bef1
Cutaku <mmzz0219@gmail.com>
2025-11-29 19:54:21 +0900

    feat: 레포지토리 메서드 추가

--- src/main/java/site/treetory/domain/member/repository/MemberRepository.java
+++ src/main/java/site/treetory/domain/member/repository/MemberRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import site.treetory.domain.member.entity.Member;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUuid(String uuid);
+
+    Optional<Member> findByEmail(String email);
 }
