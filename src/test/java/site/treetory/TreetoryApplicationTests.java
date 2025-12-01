56ec01d3d92b5aaf35ff2fc0fbbc250b15b61180
dwd9999 <ojw1010@gmail.com>
2025-12-01 20:12:20 +0900

    test: 테스트 설정파일 분리

--- src/test/java/site/treetory/TreetoryApplicationTests.java
+++ src/test/java/site/treetory/TreetoryApplicationTests.java
@@ -3,9 +3,11 @@ package site.treetory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import site.treetory.global.config.TestContainersConfig;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Import(TestContainersConfig.class)
 class TreetoryApplicationTests {
 
