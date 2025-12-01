40319b65c72122f081cb31ab5ef854b48717bfb5
dwd9999 <ojw1010@gmail.com>
2025-12-01 19:46:18 +0900

    test: DB TestContainer 세팅

--- src/test/java/site/treetory/TreetoryApplicationTests.java
+++ src/test/java/site/treetory/TreetoryApplicationTests.java
@@ -2,8 +2,11 @@ package site.treetory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import site.treetory.global.config.TestContainersConfig;
 
 @SpringBootTest
+@Import(TestContainersConfig.class)
 class TreetoryApplicationTests {
 
 	@Test
---
+++ src/test/java/site/treetory/global/config/TestContainersConfig.java
@@ -0,0 +1,24 @@
+package site.treetory.global.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration
+public class TestContainersConfig {
+    
+    @Bean
+    @ServiceConnection
+    public MySQLContainer<?> mySQLContainer() {
+        return new MySQLContainer<>(DockerImageName.parse("mysql:8.4"))
+                .withDatabaseName("treetory");
+    }
+    
+    @Bean
+    public GenericContainer<?> redisContainer() {
+        return new GenericContainer<>(DockerImageName.parse("redis:7.0"));
+    }
+}
