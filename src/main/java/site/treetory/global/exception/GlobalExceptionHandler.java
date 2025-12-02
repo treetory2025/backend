71bed5722eee0b4bba5a48a905cc490182eb4875
Cutaku <mmzz0219@gmail.com>
2025-12-02 12:58:41 +0900

    feat: validation exception 처리

--- src/main/java/site/treetory/global/exception/GlobalExceptionHandler.java
+++ src/main/java/site/treetory/global/exception/GlobalExceptionHandler.java
@@ -1,8 +1,13 @@
 package site.treetory.global.exception;
 
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import site.treetory.global.dto.ResponseDto;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +20,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
     protected ResponseDto<?> handleCustomException(CustomException e) {
         return ResponseDto.fail(e.getErrorCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseDto<?> handleValidationException(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseDto.fail(errors.toString());
     }
 }
