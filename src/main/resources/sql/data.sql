1bff840d7cad99c411d605de5b09da2a84fe4182
dwd9999 <ojw1010@gmail.com>
2025-12-02 15:40:08 +0900

    feat: 오너먼트 테스트 데이터 추가

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,5 +1,13 @@
-insert into member (member_id, uuid, email, nickname, role, is_deleted, created_at, modified_at) values
-        (1, 'b8a3eb59-b956-4df9-8a55-80784016b8d4', 'test@test.com', '테스트유저', 'ROLE_USER', false, current_timestamp, current_timestamp);
+insert into member (member_id, uuid, email, nickname, role, is_deleted, created_at, modified_at)
+values (1, 'b8a3eb59-b956-4df9-8a55-80784016b8d4', 'test@test.com', '테스트유저', 'ROLE_USER', false, current_timestamp,
+        current_timestamp);
 
-insert into tree (member_id, size, theme, background) values
-                                                          (1, 1, 'SNOWY', 'SILENT_NIGHT');
+insert into tree (member_id, size, theme, background)
+values (1, 1, 'SNOWY', 'SILENT_NIGHT');
+
+insert into ornament (ornament_id, name, category, img_url, is_public, is_deleted, created_at, modified_at)
+values (1, 'name', 'CHRISTMAS', 'urlurl', true, false, current_timestamp, current_timestamp);
+
+insert into placed_ornament (placed_ornament_id, tree_id, ornament_id, position_x, position_y, message, writer_nickname,
+                             font, is_deleted, created_at, modified_at)
+values (1, 1, 1, 1, 1, 'message', 'nickni', 'NANUM_HANDWRITING', false, current_timestamp, current_timestamp)
