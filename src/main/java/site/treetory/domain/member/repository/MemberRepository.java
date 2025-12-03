3e9fbd9cd8e081d33d82b6f1c453725efa27ffcc
Cutaku <107826896+Cutaku@users.noreply.github.com>
2025-12-03 22:39:17 +0900

    feat: 회원 검색 기능 구현 (#13)
    * feat: 본인 정보 조회 api 엔드포인트 수정
    
    * feat: page dto 추가
    
    * feat: 회원 검색 api 구현

--- src/main/java/site/treetory/domain/member/repository/MemberRepository.java
+++ src/main/java/site/treetory/domain/member/repository/MemberRepository.java
@@ -1,7 +1,12 @@
 package site.treetory.domain.member.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import site.treetory.domain.member.dto.MemberWithOrnamentsCount;
 import site.treetory.domain.member.entity.Member;
 
 import java.util.Optional;
@@ -12,4 +17,14 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUuid(String uuid);
 
     Optional<Member> findByEmail(String email);
+
+    @Query("select new site.treetory.domain.member.dto.MemberWithOrnamentsCount(" +
+            "   m.uuid, m.nickname, m.email, count(p)) " +
+            "from Member m " +
+            "left join PlacedOrnament p " +
+            "on m.id = p.tree.id " +
+            "where m.nickname like :query or m.email like :query " +
+            "group by m.id " +
+            "order by count(p) desc")
+    Page<MemberWithOrnamentsCount> findMembersByQuery(@Param("query") String query, Pageable pageable);
 }
--- src/main/java/site/treetory/domain/member/service/MemberService.java
+++ src/main/java/site/treetory/domain/member/service/MemberService.java
@@ -1,10 +1,15 @@
 package site.treetory.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import site.treetory.domain.member.dto.MemberWithOrnamentsCount;
 import site.treetory.domain.member.dto.req.ChangeNicknameReq;
+import site.treetory.domain.member.dto.req.SearchMembersReq;
 import site.treetory.domain.member.dto.res.MemberDetailsRes;
+import site.treetory.domain.member.dto.res.SearchMembersRes;
 import site.treetory.domain.member.entity.Bookmark;
 import site.treetory.domain.member.entity.BookmarkId;
 import site.treetory.domain.member.entity.Member;
@@ -31,6 +36,15 @@ public class MemberService {
                 .orElseThrow(() -> new CustomException(NOT_FOUND));
 
         return MemberDetailsRes.toDto(member, tree);
+    }
+
+    public SearchMembersRes searchMembers(SearchMembersReq searchMembersReq, Pageable pageable) {
+
+        String query = searchMembersReq.getQuery();
+
+        Page<MemberWithOrnamentsCount> members = memberRepository.findMembersByQuery(query, pageable);
+
+        return new SearchMembersRes(members);
     }
 
     @Transactional
