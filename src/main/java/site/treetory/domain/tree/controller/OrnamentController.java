bbc45b8ade519c4542bbf626b9dcb1dbd6793926
dwd9999 <ojw1010@gmail.com>
2025-12-05 20:26:10 +0900

    feat: 오너먼트 관련 컨트롤러 구현

---
+++ src/main/java/site/treetory/domain/tree/controller/OrnamentController.java
@@ -0,0 +1,46 @@
+package site.treetory.domain.tree.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import site.treetory.domain.member.entity.Member;
+import site.treetory.domain.tree.dto.req.AddOrnamentReq;
+import site.treetory.domain.tree.dto.res.OrnamentListRes;
+import site.treetory.domain.tree.dto.res.OrnamentNameExistsRes;
+import site.treetory.domain.tree.service.OrnamentService;
+import site.treetory.global.argument_resolver.LoginMember;
+import site.treetory.global.dto.ResponseDto;
+
+import static site.treetory.global.statuscode.SuccessCode.CREATED;
+import static site.treetory.global.statuscode.SuccessCode.OK;
+
+@RestController
+@RequestMapping("/api/ornaments")
+@RequiredArgsConstructor
+public class OrnamentController {
+
+    private final OrnamentService ornamentService;
+
+    @GetMapping
+    public ResponseDto<OrnamentListRes> getOrnaments() {
+        OrnamentListRes result = ornamentService.getOrnaments();
+
+        return ResponseDto.success(OK, result);
+    }
+
+    @PostMapping
+    public ResponseDto<Void> addOrnament(@LoginMember Member member,
+                                         @Valid @RequestBody AddOrnamentReq addOrnamentReq) {
+        ornamentService.addOrnament(addOrnamentReq);
+
+        return ResponseDto.success(CREATED);
+    }
+
+    @GetMapping("/exists")
+    public ResponseDto<OrnamentNameExistsRes> checkOrnamentNameExists(@LoginMember Member member,
+                                                                      @RequestParam String name) {
+        OrnamentNameExistsRes result = ornamentService.checkOrnamentNameExists(name);
+
+        return ResponseDto.success(OK, result);
+    }
+}
