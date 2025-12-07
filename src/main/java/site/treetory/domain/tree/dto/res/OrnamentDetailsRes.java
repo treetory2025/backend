65d7a85b788d57f244408703cf2215b685faeb1b
dwd9999 <ojw1010@gmail.com>
2025-12-08 00:48:25 +0900

    feat: 오너먼트 상세 조회 API DTO 추가

---
+++ src/main/java/site/treetory/domain/tree/dto/res/OrnamentDetailsRes.java
@@ -0,0 +1,31 @@
+package site.treetory.domain.tree.dto.res;
+
+import lombok.Builder;
+import lombok.Getter;
+import site.treetory.domain.tree.entity.Ornament;
+import site.treetory.global.util.DateFormatter;
+
+@Getter
+@Builder
+public class OrnamentDetailsRes {
+
+    private String name;
+
+    private String category;
+
+    private String imgUrl;
+
+    private String userNickname;
+
+    private String createdDate;
+
+    public static OrnamentDetailsRes toDto(Ornament ornament) {
+        return OrnamentDetailsRes.builder()
+                .name(ornament.getName())
+                .category(ornament.getCategory().getDescription())
+                .imgUrl(ornament.getImgUrl())
+                .userNickname(ornament.getMember().getNickname())
+                .createdDate(DateFormatter.convertToDate(ornament.getCreatedAt()))
+                .build();
+    }
+}
